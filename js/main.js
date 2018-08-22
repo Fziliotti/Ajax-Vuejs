c6bfe4d64869f852589b8dc833abd187ff896888
Fabricio Ziliotti <ffz.ziliotti@gmail.com>
2018-08-22 11:24:17 -0300

    adicionando AXIOS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-	var mixin = {
+	// USANDO O VUE RESOURCE
+	var ajaxPosts = {
 		methods: {
-			getUsers(){
+			getPosts(){
 				this.loader.posts = true;
 				var url = 'https://jsonplaceholder.typicode.com/posts';
 					// quando essa requisicao terminar, entao vamos realizar uma ação
@@ -18,14 +19,36 @@
 			}
 		}
 
+
+		// USANDO AXIOS
+		var ajaxPhotos = {
+			methods: {
+				getPhotos(){
+					this.loader.photos = true;
+					var url = 'https://jsonplaceholder.typicode.com/photos';
+					// quando essa requisicao terminar, entao vamos realizar uma ação
+					var self = this;
+					axios.get(url).then(function(response){
+						self.photos = response.data;
+						self.loader.photos = false;
+					});
+
+
+					
+				}
+			}
+		}
+
 		var app =  new Vue({
 			el:"#app",
-			mixins: [mixin],
+			mixins: [ajaxPosts,ajaxPhotos],
 			data: {
 				titulo: "Vue js com AJAX",
 				posts: [],
+				photos: [],
 				loader: {
 					posts:false,
+					photos: false
 				},
 				response: {
 					msg: "Erro ao conectar, tente novamente mais tarde.",
